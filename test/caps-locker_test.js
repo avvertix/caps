a9d2650a9dc67c5870b98bd49828b7f6bc1bf798
alessio <alessio.vertemati@gmail.com>
2015-09-27 21:07:19 +0200

    Closes #1 - added Function.bind shim

--- test/caps-locker_test.js
+++ test/caps-locker_test.js
@@ -3,6 +3,35 @@
   module('caps', {
     setup: function () {
       // this.elems = $('#qunit-fixture').children();
+      
+      // fixes the case that the bind method is not available in the headless browser
+      if (!Function.prototype.bind) {
+        Function.prototype.bind = function(oThis) {
+          if (typeof this !== 'function') {
+            // closest thing possible to the ECMAScript 5
+            // internal IsCallable function
+            throw new TypeError('Function.prototype.bind - what is trying to be bound is not callable');
+          }
+      
+          var aArgs   = Array.prototype.slice.call(arguments, 1),
+              fToBind = this,
+              FNOP    = function() {},
+              fBound  = function() {
+                return fToBind.apply(this instanceof FNOP ? this : oThis,
+                      aArgs.concat(Array.prototype.slice.call(arguments)));
+              };
+      
+          if (this.prototype) {
+            // native functions don't have a prototype
+            FNOP.prototype = this.prototype; 
+          }
+          fBound.prototype = new FNOP();
+      
+          return fBound;
+        };
+      }
+      
+      
     }
   });
 
